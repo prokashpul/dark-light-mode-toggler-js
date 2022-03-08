aaec26a12bfcf2398ac8aeb5ef4369b75a4120d5
Prokash Pul <pprokash102@gmail.com>
2022-03-08 21:01:03 +0600

    add click event add set data localstorege toggle derk mode

---
+++ js/app.js
@@ -0,0 +1,31 @@
+// display dark mode toggle
+
+// get local storage 
+let darkMode = localStorage.getItem('darkMode');
+// create dark mode enable function
+
+const darkModeEnable = () => {
+    localStorage.setItem('darkMode', 'dark');
+    // class add
+    document.body.classList.add('dark');
+}
+
+const darkModeDisable = () => {
+    localStorage.setItem('darkMode', 'light');
+    // class add
+    document.body.classList.remove('dark');
+}
+
+
+// button click event dark mode toggle
+
+const darkThem = () => {
+    darkMode = localStorage.getItem('darkMode');
+    if (darkMode !== 'dark') {
+        darkModeEnable();
+        document.getElementById('darkLight').innerText = 'Light';
+    } else {
+        darkModeDisable();
+        document.getElementById('darkLight').innerText = 'Dark';
+    }
+}
